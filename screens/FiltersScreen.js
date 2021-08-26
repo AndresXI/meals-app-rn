f349cc574ddc6b60f8c447c430a65faee7df9167
andres alcocer <andresalcocer7@yahoo.com>
2021-08-26 13:25:59 -0400

    created filter screen content

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
 import HeaderButton from '../components/HeaderButton'
+import Colors from '../constants/Colors'
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+        trackColor={{ true: Colors.primaryColor }}
+        onValueChange={props.onChange}
+        value={props.state}
+      />
+    </View>
+  )
+}
 
 const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false)
+  const [isLactoseFree, setIsLactoseFree] = useState(false)
+  const [isVegan, setIsVegan] = useState(false)
+  const [isVegetarian, setIsVegetarian] = useState(false)
+
   return (
     <View style={styles.screen}>
-      <Text>Filters Screen</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label={'Gluten-free'}
+        state={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label={'Lactose-free'}
+        state={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label={'Vegan'}
+        state={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label={'Vegetarian'}
+        state={isVegetarian}
+        onChange={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   )
 }
@@ -32,8 +73,19 @@ FiltersScreen.navigationOptions = (navData) => {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 15,
   },
 })
